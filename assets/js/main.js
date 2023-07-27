dee6ec8aedfa3b11b011a106bfd90b9f4555a032
dcthoai <dcthoai1023@gmail.com>
2023-07-27 09:02:07 +0700

    add label for song playing

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,21 +3,22 @@ const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
 const dashboard = $('.dashboard');
+const cd = $('.dashboard .cd');
 const cdThumb = $('.dashboard .cd .cd__thumb');
 const nameSongPlaying = $('.dashboard .song-playing .name-current-song');
 const nameSingerPlaying = $('.dashboard .song-playing .name-singer');
 const playSongBtn = $('.dashboard .controls .controls__play');
-const loopSongBtn = $('.dashboard .controls .fa-rotate-right');
+const repeatSongBtn = $('.dashboard .controls .fa-rotate-right');
 const backSongBtn = $('.dashboard .controls .fa-backward-step');
 const nextSongBtn = $('.dashboard .controls .fa-forward-step');
-const shuffleSongBtn = $('.dashboard .controls .fa-shuffle');
+const randomSongBtn = $('.dashboard .controls .fa-shuffle');
 const timeSongInput = $('.dashboard #progress');
 const audio = $('.dashboard audio');
 
 const appPlayer = {
     currentIndexSong: 0,
     isPlaying: false,
-    isLoopSong: false,
+    isRepeatSong: false,
     isRandomSong: false,
 
     songs: [
@@ -25,68 +26,68 @@ const appPlayer = {
             name: 'Thanh Ti',
             singer: 'Đẳng Thập Ma Quân',
             path: './assets/music/thanh-ti.mp3',
-            image: './assets/img/thanh-ti-avartar.jpg'
+            image: './assets/img/thanh-ti.jpg'
         },
         {
             name: 'Cánh Đồng Yêu Thương',
             singer: 'Trung Quân',
             path: './assets/music/canh-dong-yeu-thuong.mp3',
-            image: './assets/img/canh-dong-yeu-thuong-avartar.jpg'
+            image: './assets/img/canh-dong-yeu-thuong.jpg'
         },
         {
             name: 'Loving You Sunny',
             singer: 'Đen Vâu x Kimmese',
             path: './assets/music/loving-you-sunny.mp3',
-            image: './assets/img/loving-you-sunny-avartar.jpg'
+            image: './assets/img/loving-you-sunny.jpg'
         },
         {
             name: 'Chớ Hỏi Biệt Ly x Ciaga',
             singer: 'Chỉ Tiêm Tiếu',
             path: './assets/music/cho-hoi-biet-ly-and-ciaga.mp3',
-            image: './assets/img/cho-hoi-biet-ly-avartar.jpg'
+            image: './assets/img/cho-hoi-biet-ly-and-ciaga.jpg'
         },
         {
             name: 'Hồng Nhan Xưa',
             singer: 'Lưu Đào',
             path: './assets/music/hong-nhan-xua.mp3',
-            image: './assets/img/avatar-hong-nhan-xua.jpg'
+            image: './assets/img/hong-nhan-xua.jpg'
         },
         {
             name: 'Đáy Biển',
             singer: 'Nhất Chi Lựu Liên',
             path: './assets/music/day-bien.mp3',
-            image: './assets/img/avatar-day-bien.jpg'
+            image: './assets/img/day-bien.jpg'
         },
         {
             name: 'Wolves',
             singer: 'Selena Gomez',
             path: './assets/music/wolves.mp3',
-            image: './assets/img/wolves-avartar.jpg'
+            image: './assets/img/wolves.jpg'
         },
         {
             name: 'Close To The Sun',
             singer: 'TheFatRat & Anjulie',
             path: './assets/music/close-to-the-sun.mp3',
-            image: './assets/img/close-to-the-sun-avartar.jpg'
+            image: './assets/img/close-to-the-sun.jpg'
         },
         {
             name: 'We\'ll Meet Again',
             singer: 'TheFatRat & Laura Brehm',
             path: './assets/music/well-meet-again.mp3',
-            image: './assets/img/we-will-meet-again-avarta.jpg'
+            image: './assets/img/we-will-meet-again.jpg'
         },
         {
             name: 'Light It Up & Rise',
             singer: 'NCS',
             path: './assets/music/light-it-up-x-rise.mp3',
-            image: './assets/img/song-icon.jpg'
+            image: './assets/img/default-song-icon.jpg'
         },
     ],
 
     renderSong: function(){
-        var html = this.songs.map(function(song){
+        var html = this.songs.map(function(song, index){
             return `
-                <div class="song">
+                <div class="song data${index}">
                     <div class="song__avatar"><img src="${song.image}" alt=""></div>
                     <div class="song__info">
                         <div class="song__info-name">${song.name}</div>
@@ -104,20 +105,24 @@ const appPlayer = {
         nameSingerPlaying.innerHTML = `${this.songs[this.currentIndexSong].singer}`;
         cdThumb.style.backgroundImage = `url('${this.songs[this.currentIndexSong].image}')`;
         audio.src = `${this.songs[this.currentIndexSong].path}`;
+        
+        if($('.play-list .active'))
+            $('.play-list .active').classList.remove('active');
+        $(`.play-list .data${this.currentIndexSong}`).classList.add('active');
     },
     
     handleScroll: function(){
-        const cdWidth = $('.dashboard .cd').offsetWidth;
         const player = $('.player');
-        const cdThumb = $('.cd');
-
+        const cdWidth = cd.offsetWidth;
+        // zoom cdThumb when scroll play-list
         player.onscroll = function(){
             const scrollTop = player.scrollY || player.scrollTop;
-            var newCdWidth = cdWidth - scrollTop;
+            let newCdWidth = cdWidth - scrollTop;
             newCdWidth = newCdWidth < 0 ? 0 : newCdWidth;
-            cdThumb.style.width = newCdWidth + 'px';
-            cdThumb.style.height = newCdWidth + 'px';
-            cdThumb.style.opacity = newCdWidth / cdWidth;
+
+            cd.style.width = newCdWidth + 'px';
+            cd.style.height = newCdWidth + 'px';
+            cd.style.opacity = newCdWidth / cdWidth;
         }
     },
 
@@ -127,7 +132,6 @@ const appPlayer = {
         else
             this.currentIndexSong = 0;
         this.loadCurrentSong();
-        audio.play();
     },
 
     backSong: function(){
@@ -136,50 +140,51 @@ const appPlayer = {
         else
             this.currentIndexSong = this.songs.length - 1;
         this.loadCurrentSong();
-        audio.play();
     },
 
-    playRandomSong: function(){
-        this.currentIndexSong = Math.floor(Math.random() * this.songs.length);
+    randomSong: function(){
+        let newIndexSong;
+        do{
+            newIndexSong = Math.floor(Math.random() * this.songs.length);
+        } while (newIndexSong === this.currentIndexSong);
+        this.currentIndexSong = newIndexSong;
         this.loadCurrentSong();
-        audio.play();
     },
 
-    onLoopPlaying: function(){
-        if(this.isLoopSong){
+    onRepeatPlaying: function(){
+        if(this.isRepeatSong){
             audio.loop = false;
-            this.isLoopSong = false;
-            loopSongBtn.style.color = '#8400ff';
+            this.isRepeatSong = false;
+            repeatSongBtn.style.color = '#8400ff';
         }else{
             audio.loop = true;
-            this.isLoopSong = true;
-            loopSongBtn.style.color = '#ff0000';
+            this.isRepeatSong = true;
+            repeatSongBtn.style.color = '#ff0000';
             if(this.isRandomSong){
                 this.isRandomSong = false;
-                shuffleSongBtn.style.color = '#8400ff';
+                randomSongBtn.style.color = '#8400ff';
             }
         }
     },
 
     onRandomPlaying: function(){
         if(this.isRandomSong){
-            shuffleSongBtn.style.color = '#8400ff';
+            randomSongBtn.style.color = '#8400ff';
             this.isRandomSong = false;
         }else{
-            shuffleSongBtn.style.color = '#ff0000';
+            randomSongBtn.style.color = '#ff0000';
             this.isRandomSong = true;
-            if(this.isLoopSong){
+            if(this.isRepeatSong){
                 audio.loop = false;
-                this.isLoopSong = false;
-                loopSongBtn.style.color = '#8400ff';
+                this.isRepeatSong = false;
+                repeatSongBtn.style.color = '#8400ff';
             }
         }
     },
 
-    handleEvents: function(){
-        const app = this;
-        const listSongs = $$('.play-list .song');
-        const cdThumbRotate = cdThumb.animate(
+    onPlayingSong: function(){
+        // animation rotate for cdThumb
+        const cdThumbRotate = cdThumb.animate(  
             [
                 {transform: 'rotate(360deg)'}
             ],  
@@ -190,13 +195,14 @@ const appPlayer = {
         )
         cdThumbRotate.pause();
 
+        // Play or pause music when click on playSongBtn
         playSongBtn.onclick = function(){
-            if(app.isPlaying){
+            if(this.isPlaying){
                 audio.pause();
-                app.isPlaying = false;
+                this.isPlaying = false;
             }else{
                 audio.play();
-                app.isPlaying = true;
+                this.isPlaying = true;
             }
         };
         audio.onplay = function(){
@@ -208,40 +214,55 @@ const appPlayer = {
             cdThumbRotate.pause();
         };
         
+        // Auto update time of song playing
         audio.ontimeupdate = function(){
             if(audio.duration > 0){
-                timeSongInput.value = Math.floor((audio.currentTime / audio.duration) * 1000);
+                timeSongInput.value = Math.floor((audio.currentTime / audio.duration) * 100);
             }else{
                 timeSongInput.value = 0;
             }
         };
 
+        // choose any position in song to playing
         timeSongInput.onchange = function(){
-            audio.currentTime = Math.floor((timeSongInput.value * audio.duration) / 1000);
+            audio.currentTime = Math.floor((timeSongInput.value * audio.duration) / 100);
             audio.play();
         };
+    },
 
-        loopSongBtn.onclick = function(){
-            app.onLoopPlaying();
+    handleEvents: function(){
+        const app = this;
+        const listSongs = $$('.play-list .song');
+
+        app.onPlayingSong();
+
+        // Turn on song repeat mode
+        repeatSongBtn.onclick = function(){
+            app.onRepeatPlaying();
         };
-        shuffleSongBtn.onclick = function(){
+        // Turn on song random mode
+        randomSongBtn.onclick = function(){
             app.onRandomPlaying();
         };
-        
+        // Skip to next song
         nextSongBtn.onclick = function(){
             app.nextSong();
         };
+        // Skip to previous song
         backSongBtn.onclick = function(){
             app.backSong();
         };
 
+        // Choose song play mode when it ended
         audio.addEventListener('ended', function(){
             if(app.isRandomSong)
-                app.playRandomSong();
+                app.randomSong();
             else
                 app.nextSong();
+            audio.play();
         });
 
+        // Add event play song when click it
         listSongs.forEach(function(song, indexSong){
             song.onclick = function(){
                 app.currentIndexSong = indexSong;
